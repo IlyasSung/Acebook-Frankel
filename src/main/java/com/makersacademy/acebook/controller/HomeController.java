5d2c61f257607f19384e71152162e3edf61dc92d
Ella Skolnick <ellaskolnick@me.com>
2022-11-24 13:28:24 +0100

    home page

--- src/main/java/com/makersacademy/acebook/controller/HomeController.java
+++ src/main/java/com/makersacademy/acebook/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.makersacademy.acebook.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
@@ -7,7 +9,10 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 public class HomeController {
 	@RequestMapping(value = "/")
-	public RedirectView index() {
-		return new RedirectView("/posts");
+	public String index(Principal principal) {
+    if (principal != null) {
+      return "redirect:/posts";
+    }
+		return "home/home";
 	}
 }
